7e2b7679dbe01677f17a11903d4c032218078f02
panghae <yds05074@gmail.com>
2024-01-16 13:10:14 +0900

    fix: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import * as path from 'path';
+import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), svgr({ include: '**/*.svg?react' })],
+  resolve: {
+    extensions: ['.js', '.ts', '.jsx', '.tsx'],
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    outDir: 'build',
+  },
+});
